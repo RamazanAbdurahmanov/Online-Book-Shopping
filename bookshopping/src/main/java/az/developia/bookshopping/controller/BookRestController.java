9e7146c6df78f9a3ee174509a572585ec746e491
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-03-04 18:06:15 +0400

    015 Book Shopping
    Kitab uchun tesvir elave edildi

---
+++ bookshopping/src/main/java/az/developia/bookshopping/controller/BookRestController.java
@@ -0,0 +1,32 @@
+package az.developia.bookshopping.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import az.developia.bookshopping.dao.BookDAO;
+import az.developia.bookshopping.model.Book;
+
+@RestController
+@RequestMapping(path="/rest/books")
+public class BookRestController {
+	@Autowired
+	private BookDAO bookDAO;
+	
+	
+	@GetMapping
+	public List<Book>findAll(){
+		return bookDAO.findAll();
+	}
+	
+	@GetMapping(path="/{id}")
+	public Book findById(@PathVariable(name="id")Integer id) {
+		return bookDAO.findById(id).get(); 
+	}
+	
+
+}
