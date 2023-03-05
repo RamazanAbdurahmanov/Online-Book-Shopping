a51763fbd96013417ffbb58abfd998930bcb167e
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-03-05 09:46:55 +0400

    024 Kitabin redaktesi 2 ci addim

--- bookshopping/src/main/java/az/developia/bookshopping/controller/BookController.java
+++ bookshopping/src/main/java/az/developia/bookshopping/controller/BookController.java
@@ -16,69 +16,67 @@ import az.developia.bookshopping.model.Book;
 
 @Controller
 public class BookController {
-@Autowired
-private BookDAO bookDAO;
+	@Autowired
+	private BookDAO bookDAO;
 
-@GetMapping(path="books")
-public String showBooks(Model model) {
-	List<Book> books=bookDAO.findAll();
-	model.addAttribute("books",books);
-	return "books";
-}
-@GetMapping(path="/books/new")
-public String openNewBookPage(Model model) {
-	Book book=new Book();
-	model.addAttribute("book",book);
-	return "new-book";
-	
-	
-}
-@PostMapping(path="/books/new-book-process")
-public String saveBook(@ModelAttribute(name="book")Book book,Model model) {
-	book.setImage("book.jpg");
-	book.setUsername("dea");
-bookDAO.save(book);
-List<Book>books=bookDAO.findAll();
-model.addAttribute("books",books);
-	 
-	return "books";
-	
-}
-@GetMapping(path="/books/delete/{id}")
-public String deleteBook(@PathVariable(name="id")Integer id,Model model) {
-	boolean bookExists=bookDAO.findById(id).isPresent();
-	if(bookExists) {
-		bookDAO.deleteById(id);
-	}else {
-		
+	@GetMapping(path = "books")
+	public String showBooks(Model model) {
+		List<Book> books = bookDAO.findAll();
+		model.addAttribute("books", books);
+		return "books";
 	}
-	
-	
-	List<Book>books=bookDAO.findAll();
-	model.addAttribute("books",books);
-	
-	return "redirect:/books";
 
-}
+	@GetMapping(path = "/books/new")
+	public String openNewBookPage(Model model) {
+		Book book = new Book();
+		model.addAttribute("book", book);
+		return "new-book";
 
-
-@GetMapping(path="/books/edit/{id}")
-public String editBook(@PathVariable(name="id")Integer id,Model model) {
-	Optional<Book> bookOptional=bookDAO.findById(id);
-	boolean bookExists=bookOptional.isPresent();
-	Book book=new Book();
-	if(bookExists) {
-		book=bookOptional.get();
-	}else {
-		
 	}
-	
-	
-	model.addAttribute("book",book);
-	
-	return "new-book";
 
-}
+	@PostMapping(path = "/books/new-book-process")
+	public String saveBook(@ModelAttribute(name = "book") Book book, Model model) {
+		book.setImage("book.jpg");
+		book.setUsername("dea");
+		bookDAO.save(book);
+		List<Book> books = bookDAO.findAll();
+		model.addAttribute("books", books);
 
+		return "books";
+
+	}
+
+	@GetMapping(path = "/books/delete/{id}")
+	public String deleteBook(@PathVariable(name = "id") Integer id, Model model) {
+		boolean bookExists = bookDAO.findById(id).isPresent();
+		if (bookExists) {
+			bookDAO.deleteById(id);
+		} else {
+
+		}
+
+		List<Book> books = bookDAO.findAll();
+		model.addAttribute("books", books);
+
+		return "redirect:/books";
+
+	}
+
+	@GetMapping(path = "/books/edit/{id}")
+	public String editBook(@PathVariable(name = "id") Integer id, Model model) {
+		Optional<Book> bookOptional = bookDAO.findById(id);
+		boolean bookExists = bookOptional.isPresent();
+		Book book = new Book();
+		if (bookExists) {
+			book = bookOptional.get();
+		} else {
+
+		}
+
+		model.addAttribute("book", book);
+
+		return "new-book";
+
+	}
 
 }
