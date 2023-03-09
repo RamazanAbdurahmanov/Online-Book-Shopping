fd600ee76c3eeb1b0dafffeb012df72db5737035
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-03-09 23:08:14 +0400

    056 Istifadecinin bazada qeydiyyati

--- bookshopping/src/main/java/az/developia/bookshopping/config/SecurityConfig.java
+++ bookshopping/src/main/java/az/developia/bookshopping/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package az.developia.bookshopping.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,5 +21,12 @@ protected void configure(HttpSecurity http) throws Exception {
  .loginProcessingUrl("/authenticate-user").permitAll()
  .and().logout().permitAll();
 
+}
+
+@Autowired
+DataSource dataSource;
+@Override
+protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+auth.jdbcAuthentication().dataSource(dataSource);
 }
 }
