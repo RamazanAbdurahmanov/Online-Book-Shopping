90c817d4b2c9920afe41262f54594bdf40d1aef7
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-03-11 15:43:05 +0400

    061 Yeni istifadechi yaratmaq son addim

--- bookshopping/src/main/java/az/developia/bookshopping/controller/UserController.java
+++ bookshopping/src/main/java/az/developia/bookshopping/controller/UserController.java
@@ -3,6 +3,7 @@ package az.developia.bookshopping.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,10 +11,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import az.developia.bookshopping.dao.UserDAO;
 import az.developia.bookshopping.model.User;
 
 @Controller
 public class UserController {
+	@Autowired
+	private UserDAO userDAO;
+	
+	
+	
+	
+	
 	private boolean userCreated=false;
 	
 	@GetMapping(path="/show-login")
@@ -39,7 +48,12 @@ public class UserController {
 		if(result.hasErrors()) {
 			return "create-account";
 		}
-		System.out.println(user);
+		//System.out.println(user);
+		boolean userExists=userDAO.createUser(user);
+		if(userExists) {
+			model.addAttribute("userExists","");
+			return "create-account";
+		}
 		userCreated=true;
 	return "redirect:/show-login";
 
