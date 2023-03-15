1111be68afcb2475a27f8c5bcbdd14dbfd3695db
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-03-15 23:46:55 +0400

    075.Fayl yuklemek uchun qovlugun teyini

--- bookshopping/src/main/java/az/developia/bookshopping/file/StorageProperties.java
+++ bookshopping/src/main/java/az/developia/bookshopping/file/StorageProperties.java
@@ -1,5 +1,12 @@
 package az.developia.bookshopping.file;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@ConfigurationProperties("storage")
+@Data
 public class StorageProperties {
+	private String location="upload-dir-book-shopping-spring-ajax";
 
 }
