48699576ccde37fc7ab9eb7d59435917a29f6956
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-03-16 20:44:20 +0400

    079.Faylin yadda saxlanilmasi metodunun  kodlari

--- bookshopping/src/main/java/az/developia/bookshopping/file/FileSystemStorageService.java
+++ bookshopping/src/main/java/az/developia/bookshopping/file/FileSystemStorageService.java
@@ -1,13 +1,17 @@
 package az.developia.bookshopping.file;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -32,8 +36,23 @@ public class FileSystemStorageService implements StorageService{
 
 	@Override
 	public String store(MultipartFile file) {
-		
-		return null;
+		String filename= StringUtils.cleanPath(file.getOriginalFilename());
+		String randomFileName="";
+		try {
+			try (InputStream inputStream=file.getInputStream()){
+				String originalFileName=file.getOriginalFilename();
+				UUID uuid=UUID.randomUUID();
+				randomFileName=originalFileName
+				.replace(originalFileName.substring(0,originalFileName.lastIndexOf(",")),
+				uuid.toString());
+				Files.copy(inputStream, this.rootLocation.resolve(randomFileName),
+				StandardCopyOption.REPLACE_EXISTING);
+			}
+			
+		} catch (IOException e) {
+			throw new StorageException("Fayl yaddana saxla bilmedi :"+filename,e);
+		}
+		return randomFileName;
 	}
 
 	@Override
