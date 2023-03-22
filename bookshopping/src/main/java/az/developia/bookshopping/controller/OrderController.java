5aaf416ef54513fe1463c7e0ab1596504fad555c
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-03-22 22:39:29 +0400

    089 Sifarishlerin siyahisi 1 ci addim

--- bookshopping/src/main/java/az/developia/bookshopping/controller/OrderController.java
+++ bookshopping/src/main/java/az/developia/bookshopping/controller/OrderController.java
@@ -1,15 +1,20 @@
 package az.developia.bookshopping.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import az.developia.bookshopping.dao.OrderDAO;
 
 @Controller
 public class OrderController {
+	@Autowired
+	private OrderDAO orderDAO;
 
 	@GetMapping(path ="/orders")
-	public String showOrdersPage() {
-
+	public String showOrdersPage(Model model) {
+        model.addAttribute("orders",orderDAO.findAll());
 		return "orders";
 
 	}
