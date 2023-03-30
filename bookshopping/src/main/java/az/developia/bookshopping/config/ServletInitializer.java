6f8addc20d63fdda5a3c28d3c8dc8e84c893d696
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-03-30 20:49:07 +0400

    104 proyektin war olaraq export edilmesi

---
+++ bookshopping/src/main/java/az/developia/bookshopping/config/ServletInitializer.java
@@ -0,0 +1,16 @@
+package az.developia.bookshopping.config;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import az.developia.bookshopping.BookshoppingApplication;
+
+public class ServletInitializer extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BookshoppingApplication.class);
+	}
+	
+
+}
